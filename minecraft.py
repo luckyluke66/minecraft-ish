c60ddd9863fd259ed618c2b71c8001f06885171d
luckyluke66 <lukasbalog66@gmail.com>
2021-11-26 20:00:46 +0100

    textury, hotbar(chybi textury)

--- minecraft.py
+++ minecraft.py
@@ -4,24 +4,49 @@ from ursina.texture_importer import load_texture
 
 app = ursina.Ursina()
 
-grass_texture = load_texture("grass_block.png")  # textury nefunguji tak jak bych si prestavoval 
+grass_texture = load_texture("grass_block.png")
+stone_texture = load_texture("stone_block.png")
+brick_texture = load_texture("brick_block.png")
+dirt_texture = load_texture("dirt_block.png")
+
+
+def update():
+    global block_pick
+    if ursina.held_keys["1"]:
+        block_pick = 1
+    if ursina.held_keys["2"]:
+        block_pick = 2
+    if ursina.held_keys["3"]:
+        block_pick = 3
+    if ursina.held_keys["4"]:
+        block_pick = 4
+
+    
+
 
 class Voxel(ursina.Button):
-    def __init__(self, position = (0,0,0)):
+    def __init__(self, position = (0,0,0), texture = grass_texture):
         super().__init__(
             parent = ursina.scene,
             position = position,
-            model = "cube",
+            model = "assets/block",
             origin_y = 0.5,
-            texture = grass_texture ,
-            color = ursina.color.white,
-            highlight_color = ursina.color.orange
+            texture = texture,
+            color = ursina.color.color(0,0,ursina.random.uniform(0.9,1)),
+            scale = 0.5
         )
     def input(self, key):
         if self.hovered:
-            if key == "left mouse down":
-                voxel = Voxel(position = self.position + ursina.mouse.normal)
             if key == "right mouse down":
+                if block_pick == 1:
+                    voxel = Voxel(position = self.position + ursina.mouse.normal, texture = grass_texture)
+                if block_pick == 2:
+                    voxel = Voxel(position = self.position + ursina.mouse.normal, texture = stone_texture)
+                if block_pick == 3:
+                    voxel = Voxel(position = self.position + ursina.mouse.normal, texture = brick_texture)
+                if block_pick == 4:
+                    voxel = Voxel(position = self.position + ursina.mouse.normal, texture = dirt_texture)
+            if key == "left mouse down":
                 ursina.destroy(self)
 
 for z in range(10):
@@ -29,5 +54,6 @@ for z in range(10):
         voxel = Voxel((x,0,z))
 
 player = FirstPersonController()
+
 
 app.run()
